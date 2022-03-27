b41f9cf53a6f6bcee8b4123a1b1091be181cb686
likeztmy <413713942@qq.com>
2022-03-27 18:08:57 +0800

    木犀周边

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-03-26 17:02:24
- * @LastEditTime: 2022-03-27 15:59:39
+ * @LastEditTime: 2022-03-27 18:03:19
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \demo\src\App.js
@@ -14,14 +14,12 @@
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \demo\src\App.js
  */
-import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import './App.css'
 
 function App() {
   return (
       <div className="wrapper" >
-        <Header />
         <Content />
       </div>
   )
